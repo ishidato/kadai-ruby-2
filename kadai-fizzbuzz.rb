da0b7a365eaf10aec2e86aaafb4b034f0dd46742
aberu <aberuking19@yahoo.co.jp>
2021-07-06 13:59:46 +0000

    aaa

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,15 +1,17 @@
 num_max = 100
-(1..num_max).each do |num|
-  def fizzbuzz(num)
-    if num % 3 == 0 && num % 5 == 0
-      puts 'FizzBuzz'
-    elsif num  % 5 == 0
-      puts 'Buzz'
-    elsif num % 3 == 0
-      puts 'Fuzz' 
-    else
-      puts num
-    end
+
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+   'FizzBuzz'
+  elsif num  % 5 == 0
+    'Buzz'
+  elsif num % 3 == 0
+    'Fuzz' 
+  else
+    num.to_s
   end
-  fizzbuzz(num)
+end
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
 end
